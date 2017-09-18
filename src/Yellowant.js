a87b92d266a8da22df1e5129906d5552cdf315b3
Manik Singh <manik@yellowant.com>
2017-09-18 18:35:57 +0530

    move client to separate file

---
+++ src/Yellowant.js
@@ -0,0 +1,35 @@
+/**
+ * A module with the main YellowAnt client class {@link module:Yellowant~Yellowant}
+ * @module
+ * @author Manik Singh <maniksingh92@gmail.com>
+ */
+
+export default class Yellowant {
+  /**
+   * YellowAnt SDK client
+   * @public
+   * 
+   * @param {Object} options Options to pass to the client
+   * @param {string} options.accessToken YellowAnt access token
+   * @param {string} options.apiUrl Default - YellowAnt API endpoint.
+   * @param {string} options.appKey YellowAnt client ID
+   * @param {string} options.appSecret YellowAnt client secret
+   * @param {Object} options.clientArgs Client arguments to be used for request headers
+   * @param {number} options.oauthVersion Version of YellowAnt OAuth to use
+   * @param {string} options.redirectUri YellowAnt redirect URI
+   * @param {string} options.tokenType Token type for oauth
+   */
+  constructor({
+    accessToken,
+    apiUrl = "https://api.yellowant.com/api/",
+    appKey,
+    appSecret,
+    redirectUri
+  }) {
+    this.accessToken = accessToken;
+    this.apiUrl = apiUrl;
+    this.appKey = appKey;
+    this.appSecret = appSecret;
+    this.redirectUri = redirectUri;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,35 +1,7 @@
-/**
- * A module with the main YellowAnt client class {@link module:index~YellowAnt}
- * @module
- * @author Manik Singh <maniksingh92@gmail.com>
- */
+import Yellowant from "./Yellowant";
+import Message from "./Message";
 
-export default class YellowAnt {
-  /**
-   * YellowAnt SDK client
-   * @public
-   * 
-   * @param {Object} options Options to pass to the client
-   * @param {string} options.accessToken YellowAnt access token
-   * @param {string} options.apiUrl Default - YellowAnt API endpoint.
-   * @param {string} options.appKey YellowAnt client ID
-   * @param {string} options.appSecret YellowAnt client secret
-   * @param {Object} options.clientArgs Client arguments to be used for request headers
-   * @param {number} options.oauthVersion Version of YellowAnt OAuth to use
-   * @param {string} options.redirectUri YellowAnt redirect URI
-   * @param {string} options.tokenType Token type for oauth
-   */
-  constructor({
-    accessToken,
-    apiUrl = "https://api.yellowant.com/api/",
-    appKey,
-    appSecret,
-    redirectUri
-  }) {
-    this.accessToken = accessToken;
-    this.apiUrl = apiUrl;
-    this.appKey = appKey;
-    this.appSecret = appSecret;
-    this.redirectUri = redirectUri;
-  }
-}
+export default {
+  Yellowant,
+  Message
+};
