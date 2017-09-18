ccd1619d71918bd7ba8213475a52e5dd9107db87
Manik Singh <manik@yellowant.com>
2017-09-18 20:20:54 +0530

    add getaccesstoken

--- src/Yellowant.js
+++ src/Yellowant.js
@@ -3,6 +3,10 @@
  * @module
  * @author Manik Singh <maniksingh92@gmail.com>
  */
+const {
+  Request
+} = require("fetch-ponyfill")();
+
 
 export default class Yellowant {
   /**
@@ -31,5 +35,30 @@ export default class Yellowant {
     this.appKey = appKey;
     this.appSecret = appSecret;
     this.redirectUri = redirectUri;
+
+
+  }
+
+  getAccessToken = (code) => {
+    const options = {
+      method: "post",
+      body: JSON.stringify({
+        grant_type: "authorization_code",
+        client_id: this.appKey,
+        client_secret: this.appSecret,
+        code,
+        redirect_uri: this.redirectUri
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    };
+
+    const request = new Request(`${this.apiUrl}oauth/request_token`, options);
+    request
+      .then(response => response.json())
+      .then(resp => {
+        console.log(resp);
+      });
   }
 }
