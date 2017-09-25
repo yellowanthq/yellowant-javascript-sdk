c96bd1a3394c30ff7b42496af97edb0e197bd520
Manik Singh <manik@yellowant.com>
2017-09-25 12:34:09 +0530

    document the Message class

--- src/Message/index.js
+++ src/Message/index.js
@@ -24,18 +24,39 @@ export default class Message {
     this.text = text;
   }
 
+  /**
+   * Add an attachment to a YellowAnt message
+   * @public
+   * 
+   * @param {MessageAttachment} attachment A MessageAttachment instance
+   */
   addAttachment = (attachment) => {
     if (attachment instanceof MessageAttachment) {
       this.attachments.push(attachment);
     }
   }
 
+  /**
+   * Add a log to a YellowAnt message
+   * @public
+   * 
+   * @param {MessageLog} log A MessageLog instance
+   */
   addLog = (log) => {
     if (log instanceof MessageLog) {
       this.logs.push(log);
     }
   }
   
+  /**
+   * Convert the message to a format recognized by YellowAnt.
+   * 
+   * Use this method before sending the data to YellowAnt
+   * @public
+   * 
+   * @return {Object} A message object fit for being sent to YellowAnt through
+   * sendMessage() or sendWebhookMessage() from the main SDK class
+   */
   toJSON = () => ({
     attachments: this.attachments.map(attachment => this.prepareAttachmentJSON(attachment)),
     data: this.data,
@@ -44,6 +65,12 @@ export default class Message {
     message_text: this.text
   })
 
+  /**
+   * Prepare the message attachment for converting to readable JSON
+   * @private
+   * 
+   * @return {Object} An attachment data in JSON object
+   */
   prepareAttachmentJSON = (attachment) => ({
     author_icon: attachment.authorIcon,
     author_link: attachment.authorLink,
@@ -63,12 +90,24 @@ export default class Message {
     ts: attachment.ts
   })
 
+  /**
+   * Prepare the message attachment field for converting to readable JSON
+   * @private
+   * 
+   * @return {Object} An attachment field data in JSON object
+   */
   prepareAttachmentFieldJSON = (attachmentField) => ({
     short: attachmentField.short,
     title: attachmentField.title,
     value: attachmentField.value
   })
 
+  /**
+   * Prepare the message attachment button for converting to readable JSON
+   * @private
+   * 
+   * @return {Object} An attachment button data in JSON object
+   */
   prepareAttachmentButtonJSON = (attachmentButton) => ({
     command: attachmentButton.command,
     name: attachmentButton.name,
