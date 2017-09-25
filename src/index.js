61c9fe7c675d36e07c8624d234a092ca1962658d
Manik Singh <manik@yellowant.com>
2017-09-25 11:59:12 +0530

    add comments for main export file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/**
+ * A module which exports all publicly available classes {@link module:Yellowant~Yellowant}
+ * @module
+ * @author Manik Singh <maniksingh92@gmail.com>
+ */
+
 import Yellowant from "./Yellowant";
 import Message from "./Message";
 import MessageAttachment from "./Message/MessageAttachment";
